e1b698408bceef7a143df50648e246e893bef06c
Michael Wenger <michaelwenger27@gmail.com>
2013-11-30 18:34:54 -0500

    Added throttle to importer to only import trials with changes since last import

--- app/controllers/importer_controller.rb
+++ app/controllers/importer_controller.rb
@@ -16,20 +16,9 @@ class ImporterController < ApplicationController
 	starting_url = "http://clinicaltrials.gov/ct2/results/download?down_stds=all&down_typ=study&recr=Open&no_unk=#{remove_unknown}&cond=#{encoded_condition}&show_down=Y"
 	# response = RestClient.get(starting_url)
 	# parsed_response = Nokogiri::XML(response)
-
-
-	# @TODO rake task route to automate this. every day. import module rake file
-	
-	# OPEN ALL XML FILES AND PARSET THEM INTO THE DATABASE
 	
 	trial_counter = 0
 	site_counter = 0
-	Dir["#{Rails.root}/public/xml_files/*.xml"].first(20).each do |file| # .first(10) to limit import
-		trial_counter += 1
-
-		f = File.open(file)
-		doc = Nokogiri::XML(f)
-		root = doc.root
 
 		# HELPER METHOD FOR IDENTIFYING DIRECTORY DEPTH
 		def get_from_xpath(path_and_name, directory, merge=false)
@@ -63,68 +52,77 @@ class ImporterController < ApplicationController
 			end
 		end 
 
-		@trial = Trial.new
-		@trial.title = get_from_xpath("brief_title",root)
-		@trial.description = get_from_xpath("brief_summary/textblock",root)
-		@trial.detailed_description = get_from_xpath("detailed_description/textblock",root)
-		@trial.sponsor = get_from_xpath("sponsors/lead_sponsor/agency",root)
-		@trial.focus = get_from_xpath("condition",root,true)
-		@trial.country = get_from_xpath("location_countries/country",root,true)  #redundant
-		@trial.nct_id = get_from_xpath("//nct_id",root)
-		@trial.official_title = get_from_xpath("official_title",root)
-		@trial.agency_class = get_from_xpath("//agency_class",root)
-		@trial.overall_status = get_from_xpath("//overall_status",root)
-		@trial.phase = get_from_xpath("//phase",root)
-		@trial.study_type = get_from_xpath("//study_type",root)
-		@trial.condition = get_from_xpath("condition",root) #redundant
-		@trial.inclusion = get_from_xpath("//criteria/textblock",root)
-		@trial.exclusion = get_from_xpath("//criteria/textblock",root)
-		@trial.gender = get_from_xpath("//gender",root)
-		@trial.healthy_volunteers = get_from_xpath("//healthy_volunteers",root)
-		@trial.overall_contact_name = get_from_xpath("//overall_contact/last_name",root)
-		@trial.overall_contact_phone = get_from_xpath("//overall_contact/phone",root)
-		@trial.overall_contact_email = get_from_xpath("//overall_contact/email",root)
-		@trial.location_countries = get_from_xpath("location_countries/country",root,true)
-		@trial.link_url = get_from_xpath("//link/url",root)
-		@trial.link_description = get_from_xpath("//link/description",root) 
-		@trial.firstreceived_date = get_from_xpath("firstreceived_date",root)
-		@trial.lastchanged_date = get_from_xpath("lastchanged_date",root)
-		@trial.verification_date = get_from_xpath("verification_date",root)
-		@trial.keyword = get_from_xpath("keyword",root,true) 
-		@trial.is_fda_regulated = get_from_xpath("is_fda_regulated",root)
-	    @trial.has_expanded_access = get_from_xpath("has_expanded_access",root)
+	Dir["#{Rails.root}/public/xml_files/*.xml"].first(20).each do |file| # .first(10) to limit import
+		f = File.open(file)
+		doc = Nokogiri::XML(f)
+		root = doc.root
+		last_import_date = Import.last.datetime
 
-		# FOR AGE: SEPARATELY STORES IMPORTED VALUE WITH MONTHS VS ALGORITHM VALUEs
-		@trial.originalminage = set_minvalue_for_age(get_from_xpath("//minimum_age",root))
-		@trial.originalmaxage = set_maxvalue_for_age(get_from_xpath("//maximum_age",root))		
-		# @trial.minimum_age = set_minvalue_for_age(get_from_xpath("//minimum_age",root))
-		# @trial.maximum_age = set_maxvalue_for_age(get_from_xpath("//maximum_age",root))
+		# SEE IF TRIAL HAS BEEN UPDATED SINCE LAST IMPORT
+		if get_from_xpath("lastchanged_date",root) < last_import_date
+			f.close
+		else
+			@trial = Trial.new
+			@trial.title = get_from_xpath("brief_title",root)
+			@trial.description = get_from_xpath("brief_summary/textblock",root)
+			@trial.detailed_description = get_from_xpath("detailed_description/textblock",root)
+			@trial.sponsor = get_from_xpath("sponsors/lead_sponsor/agency",root)
+			@trial.focus = get_from_xpath("condition",root,true)
+			@trial.country = get_from_xpath("location_countries/country",root,true)  #redundant
+			@trial.nct_id = get_from_xpath("//nct_id",root)
+			@trial.official_title = get_from_xpath("official_title",root)
+			@trial.agency_class = get_from_xpath("//agency_class",root)
+			@trial.overall_status = get_from_xpath("//overall_status",root)
+			@trial.phase = get_from_xpath("//phase",root)
+			@trial.study_type = get_from_xpath("//study_type",root)
+			@trial.condition = get_from_xpath("condition",root) #redundant
+			@trial.inclusion = get_from_xpath("//criteria/textblock",root)
+			@trial.exclusion = get_from_xpath("//criteria/textblock",root)
+			@trial.gender = get_from_xpath("//gender",root)
+			@trial.healthy_volunteers = get_from_xpath("//healthy_volunteers",root)
+			@trial.overall_contact_name = get_from_xpath("//overall_contact/last_name",root)
+			@trial.overall_contact_phone = get_from_xpath("//overall_contact/phone",root)
+			@trial.overall_contact_email = get_from_xpath("//overall_contact/email",root)
+			@trial.location_countries = get_from_xpath("location_countries/country",root,true)
+			@trial.link_url = get_from_xpath("//link/url",root)
+			@trial.link_description = get_from_xpath("//link/description",root) 
+			@trial.firstreceived_date = get_from_xpath("firstreceived_date",root)
+			@trial.lastchanged_date = get_from_xpath("lastchanged_date",root)
+			@trial.verification_date = get_from_xpath("verification_date",root)
+			@trial.keyword = get_from_xpath("keyword",root,true) 
+			@trial.is_fda_regulated = get_from_xpath("is_fda_regulated",root)
+		    @trial.has_expanded_access = get_from_xpath("has_expanded_access",root)
 
-		# doc.xpath("//location",root).each do |site|
-	     	site_counter += 1
-	 #    	@site = Site.new
-	 #    	@site.facility = get_from_xpath("facility/name",site)
-	 #    	@site.city = get_from_xpath("facility/address/city",site)
-	 #    	@site.state = get_from_xpath("facility/address/state",site)
-	 #    	@site.zip_code = get_from_xpath("facility/address/zip",site)
-	 #    	@site.country = get_from_xpath("facility/address/country",site)
-	 #    	@site.status = get_from_xpath("status",site)
+			# FOR AGE: SEPARATELY STORES IMPORTED VALUE WITH MONTHS VS ALGORITHM VALUEs
+			@trial.originalminage = set_minvalue_for_age(get_from_xpath("//minimum_age",root))
+			@trial.originalmaxage = set_maxvalue_for_age(get_from_xpath("//maximum_age",root))		
+			# @trial.minimum_age = set_minvalue_for_age(get_from_xpath("//minimum_age",root))
+			# @trial.maximum_age = set_maxvalue_for_age(get_from_xpath("//maximum_age",root))
 
-	 #    	@site.contact_name = get_from_xpath("contact/last_name",site)
-	 #    	@site.contact_phone = get_from_xpath("contact/phone",site)
-	 #    	@site.contact_phone_ext = get_from_xpath("contact/phone_ext",site)
-	 #    	@site.contact_phone_email = get_from_xpath("contact/email",site)
+			# doc.xpath("//location",root).each do |site|
+		     	site_counter += 1
+		 #    	@site = Site.new
+		 #    	@site.facility = get_from_xpath("facility/name",site)
+		 #    	@site.city = get_from_xpath("facility/address/city",site)
+		 #    	@site.state = get_from_xpath("facility/address/state",site)
+		 #    	@site.zip_code = get_from_xpath("facility/address/zip",site)
+		 #    	@site.country = get_from_xpath("facility/address/country",site)
+		 #    	@site.status = get_from_xpath("status",site)
 
-		# 	@trial.sites << @site
-		# 	@site.save
-		# end
+		 #    	@site.contact_name = get_from_xpath("contact/last_name",site)
+		 #    	@site.contact_phone = get_from_xpath("contact/phone",site)
+		 #    	@site.contact_phone_ext = get_from_xpath("contact/phone_ext",site)
+		 #    	@site.contact_phone_email = get_from_xpath("contact/email",site)
+
+			# 	@trial.sites << @site
+			# 	@site.save
+			# end
 
 
-		@trial.save
-
-	    f.close
-	    
-
+			@trial.save
+			trial_counter += 1
+		    f.close
+		end
 	  end
 
 	# TIMESTAMP THE IMPORT RUN
