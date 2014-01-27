3d7192b49177626abdcfbcbc99b3e83faab839da
Michael Wenger <michaelwenger27@gmail.com>
2014-01-26 21:32:11 -0500

    Added full ct.gov url in the notes

--- app/controllers/importer_controller.rb
+++ app/controllers/importer_controller.rb
@@ -47,7 +47,7 @@ class ImporterController < ApplicationController
   	encoded_condition = URI.encode(ClinicalTrialMatcher::Application.config.importer_query)
   	remove_unknown = ClinicalTrialMatcher::Application.config.remove_unknown
 	starting_url = "http://clinicaltrials.gov/ct2/results/download?down_stds=all&down_typ=study&recr=Open&no_unk=#{remove_unknown}&cond=#{encoded_condition}&show_down=Y"
-	`wget "#{starting_url}" -O "#{Rails.root}/tmp/ct_download.zip" `
+	`wget "#{starting_url}" -O "#{Rails.root}/tmp/ct_download.zip" ` #http://clinicaltrials.gov/ct2/results/download?down_stds=all&down_typ=study&recr=Open&no_unk=Y&cond=brain%20tumor&show_down=Y
 
 	trial_counter = 0
 	site_counter = 0
