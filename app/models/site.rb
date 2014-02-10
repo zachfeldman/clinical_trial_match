79e3c75fa119481a3fe8c3fb0272ea75962c511c
Michael Wenger <mwenger@michaeljfox.org>
2014-02-09 22:39:00 -0500

    Reenabled site geocoding

--- app/models/site.rb
+++ app/models/site.rb
@@ -3,12 +3,12 @@ class Site < ActiveRecord::Base
 	belongs_to :trial
 	
 	#TODO? City is not populating in the database.	
-	# geocoded_by :address
-	# after_validation :geocode
+	geocoded_by :address
+	after_validation :geocode
 
-	# def address
-	# 	[city, state, country, zip_code].compact.join(', ')
-	# end
+	def address
+		[city, state, country, zip_code].compact.join(', ')
+	end
 
 end
 
