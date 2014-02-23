ee9a6bfbe0bce6d9d1baa5708fccf9e69cce2ef9
mwenger1 <michaelwenger27@gmail.com>
2014-02-23 11:41:48 -0500

    Added global config for match_report recipient

--- app/mailers/newmatch.rb
+++ app/mailers/newmatch.rb
@@ -4,7 +4,7 @@ class Newmatch < ActionMailer::Base
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def new_match_report
     @user = user
-    mail( :to => @user.email,
+    mail( :to => ClinicalTrialMatcher::Application.config.import_report_recipient,
     :subject => 'New Match Report' )
   end
 end
--- config/initializers/condition.rb
+++ config/initializers/condition.rb
@@ -2,4 +2,5 @@ ClinicalTrialMatcher::Application.configure do
 	config.importer_query = "brain tumor"
 	config.filter_label = "I have a brain tumor"
 	config.remove_unknown = "Y" # Choose either Y/N
+	config.import_report_recipient = "michaelwenger27@gmail.com" # The person who will receive daily emails of what has been imported
 end
