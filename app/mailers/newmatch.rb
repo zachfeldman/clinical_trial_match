c0acc76e2ccb1d101ffa87c76bf28a2d1bca3af3
mwenger1 <michaelwenger27@gmail.com>
2014-02-23 11:40:11 -0500

    Added new_match_report action to mailer class

--- app/mailers/newmatch.rb
+++ app/mailers/newmatch.rb
@@ -1,3 +1,10 @@
 class Newmatch < ActionMailer::Base
-  default from: "from@example.com"
+  default :from => 'michaelwenger27@gmail.com'
+
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def new_match_report
+    @user = user
+    mail( :to => @user.email,
+    :subject => 'New Match Report' )
+  end
 end
