354359efad817aac90c38006bd95522db0a6af42
mwenger1 <michaelwenger27@gmail.com>
2014-02-22 20:46:15 -0500

    GH #7: Update default filter to be date last updated instead of ABC

--- app/controllers/trials_controller.rb
+++ app/controllers/trials_controller.rb
@@ -13,7 +13,7 @@ class TrialsController < ApplicationController
         end
     end
 
-    @trials = Trial.search_for(params[:q]).age(params[:age]).control?(params[:vt]).gender(params[:gender]).type(params[:ty]).phase(params[:ph]).fda(params[:fda]).focus(params[:focus]).close_to(coordinates, params[:td]).order(params[:ot]||"title ASC").paginate(:page => params[:page], :per_page => 10)
+    @trials = Trial.search_for(params[:q]).age(params[:age]).control?(params[:vt]).gender(params[:gender]).type(params[:ty]).phase(params[:ph]).fda(params[:fda]).focus(params[:focus]).close_to(coordinates, params[:td]).order(params[:ot]||"lastchanged_date DESC").paginate(:page => params[:page], :per_page => 10)
 
     # eric's refactoring recommendation -    @sites = Site.near(params[:pc],params[:td]).where(trials_ids: @trial_ids).paginate(:page => params[:page], :per_page => 10)
     session[:age] = params[:age]
