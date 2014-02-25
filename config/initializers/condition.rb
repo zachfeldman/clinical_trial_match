056dd0e76edd69ff57783d2cc2f6ed48168793c9
mwenger1 <michaelwenger27@gmail.com>
2014-02-24 23:20:36 -0500

    Updated conditions in config file

--- config/initializers/condition.rb
+++ config/initializers/condition.rb
@@ -1,5 +1,5 @@
 ClinicalTrialMatcher::Application.configure do
-	config.importer_query = "brain tumor"
-	config.filter_label = "I have a brain tumor"
+	config.importer_query = "celiac disease"
+	config.filter_label = "I have celiac disease"
 	config.remove_unknown = "Y" # Choose either Y/N
 end
